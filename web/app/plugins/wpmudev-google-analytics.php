0ad01a246b60f647fd61603cb38c783489bc62c2
Julien Ramel <julien.ramel@gmail.com>
2016-12-22 08:56:37 +0100

    fixed ga

--- web/app/plugins/wpmudev-google-analytics.php
+++ web/app/plugins/wpmudev-google-analytics.php
@@ -56,7 +56,7 @@ function wpmudev_google_analytics() { ?>
 	?>
 	<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
 	<script type="text/javascript">
-	    window.cookieconsent_options = {"message":"<?php echo __('This website uses cookies to ensure you get the best experience on our website')?>","dismiss":"<?php echo __('Got it')?>","learnMore":"<?php echo __('More info')?>","link":"<?php echo __('cookie link')?>","theme":"dark-bottom"};
+	    window.cookieconsent_options = {"message":"<?php echo __('This website uses cookies to ensure you get the best experience on our website', 'omega-td')?>","dismiss":"<?php echo __('Got it', 'omega-td')?>","learnMore":"<?php echo __('More info', 'omega-td')?>","link":"<?php echo __('cookie link', 'omega-td')?>","theme":"dark-bottom"};
 	</script>
 
 	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
